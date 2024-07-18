e84d90789fbd6be42cd1250e51e3507ef39142de
NanmorX <1063927510@qq.com>
2024-07-18 12:25:25 +0800

    菜品起售停售

--- sky-take-out/sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -92,4 +92,12 @@ public class DishController {
         dishService.updateWithFlavor(dishDTO);
         return Result.success();
     }
+
+    @PostMapping("/status/{status}")
+    @ApiOperation("起售/停售菜品")
+    public Result<String> startOrStop(@PathVariable Integer status, Long id) {
+        log.info("起售/停售菜品：{}, {}", status, id);
+        dishService.startOrStop(status, id);
+        return Result.success();
+    }
 }
