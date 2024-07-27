dd10d947933de84ed3cb58e5399ea64d0b1d5c2d
NanmorX <1063927510@qq.com>
2024-07-27 14:49:56 +0800

    跳过微信支付环节

--- sky-take-out/sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -122,20 +122,27 @@ public class OrderServiceImpl implements OrderService {
         Long userId = BaseContext.getCurrentId();
         User user = userMapper.getById(userId);
 
-        //调用微信支付接口，生成预支付交易单
-        JSONObject jsonObject = weChatPayUtil.pay(
-                ordersPaymentDTO.getOrderNumber(), //商户订单号
-                new BigDecimal(0.01), //支付金额，单位 元
-                "苍穹外卖订单", //商品描述
-                user.getOpenid() //微信用户的openid
-        );
+//        //调用微信支付接口，生成预支付交易单
+//        JSONObject jsonObject = weChatPayUtil.pay(
+//                ordersPaymentDTO.getOrderNumber(), //商户订单号
+//                new BigDecimal(0.01), //支付金额，单位 元
+//                "苍穹外卖订单", //商品描述
+//                user.getOpenid() //微信用户的openid
+//        );
 
-        if (jsonObject.getString("code") != null && jsonObject.getString("code").equals("ORDERPAID")) {
-            throw new OrderBusinessException("该订单已支付");
-        }
+//        if (jsonObject.getString("code") != null && jsonObject.getString("code").equals("ORDERPAID")) {
+//            throw new OrderBusinessException("该订单已支付");
+//        }
 
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("code", "ORDERPAID");
         OrderPaymentVO vo = jsonObject.toJavaObject(OrderPaymentVO.class);
         vo.setPackageStr(jsonObject.getString("package"));
+
+        // 代替微信支付成功后的数据库订单状态更新
+//        Integer OrderPaidStatus = Orders.PAID;
+        paySuccess(ordersPaymentDTO.getOrderNumber());
+
 
         return vo;
     }
