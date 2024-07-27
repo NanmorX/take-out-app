95cf7dcb0845091d5c63e2e674aab01e6e4ed758
NanmorX <1063927510@qq.com>
2024-07-27 13:01:15 +0800

    用户下单修改

--- sky-take-out/sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -19,6 +19,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -45,6 +46,7 @@ public class OrderServiceImpl implements OrderService {
      * @param ordersSubmitDTO
      * @return
      */
+    @Transactional
     public OrderSubmitVO submitOrder(OrdersSubmitDTO ordersSubmitDTO) {
         //异常情况的处理（收货地址为空、超出配送范围、购物车为空）
         AddressBook addressBook = addressBookMapper.getById(ordersSubmitDTO.getAddressBookId());
