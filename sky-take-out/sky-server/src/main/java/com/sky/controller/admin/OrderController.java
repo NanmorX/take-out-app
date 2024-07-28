a5a959ac774bd11af3ebf0ee88a780ac2283cb5d
NanmorX <1063927510@qq.com>
2024-07-28 14:23:05 +0800

    查询订单详情

--- sky-take-out/sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -6,11 +6,13 @@ import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
 import com.sky.vo.OrderStatisticsVO;
+import com.sky.vo.OrderVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,5 +47,18 @@ public class OrderController {
         log.info("订单数量统计");
         OrderStatisticsVO orderStatisticsVO = orderService.count();
         return Result.success(orderStatisticsVO);
+    }
+
+    /**
+     * 查询订单详情
+     * @param id
+     * @return
+     */
+    @GetMapping("/details/{id}")
+    @ApiOperation("查询订单详情")
+    public Result<OrderVO> orderDetails(@PathVariable Long id) {
+        log.info("查询订单详情: {}", id);
+        OrderVO orderVO = orderService.orderDetailQuery(id);
+        return Result.success(orderVO);
     }
 }
